f9d7c9cf0e11cb75dabef07571260e4b16cf93f8
bindupothana <himabindu.pothana@navaratan.com>
2017-03-01 15:42:26 +0530

    Custom directive

--- js/directive.js
+++ js/directive.js
@@ -3,52 +3,31 @@ app.directive("customDirective", function () {
      
     return {
         
-        restrict: "A",
+        restrict: "EA",
         
 
-        link: function (scope, element, attr) {
+        link: function ($scope, element, attr) {
            
-            element.bind('change', function() {
-                if ( this.value.length === 10 ) {
-                   var number = this.value
-                   this.value = number.substring(0,3) + '-' + number.substring(3,6) + '-' + number.substring(6,10)
+            element.bind('keydown', function(e) {
+                    if (e.keyCode == 8){
+                    
+                    }else {
+                         var number = this.value
+                    if(this.value.length == 3)
+                    {
+                        console.log("hello")
+                         this.value = number.substring(0,3) + '-';      
+                        
+                    }
+                    else if(this.value.length == 7){
+                        this.value = number.substring(0,7) + '-' ;
+                    } 
                 }
-                
-            });
            
+           });
+    
         }
+
     };
                      
 });
-/*var app=angular.module('numberApp',[])
-       app.directive('customDirective',function () {
-    var jquery = require('jquery');
-    var inputmask = require("jquery.inputmask");
-    return {
-        require: 'ngModel',
-        restrict: 'A',
-        priority: 1000,
-        link: function(scope,element, attr, ctrl) {
-
-            var jquery_element = jquery(element);
-            jquery_element.inputmask({mask:"****-****-****",autoUnmask:true});
-            jquery_element.on('keyup paste focus blur', function() {
-                var val = element.val();    
-                ctrl.$setViewValue(val);
-                ctrl.$render();
-
-             });
-
-            var pattern = /^\d{9}$/;
-
-            var newValue = null;
-
-            ctrl.$validators.ssnDigits = function(value) {
-                 newValue = element.val();
-                return newValue === '' ? true : pattern.test(newValue);    
-            };
-        }
-    };
-});
-
-*/
